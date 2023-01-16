d13599da75dafce62e02f55ceefadc0478aeee31
Ana Delic <milojevican@gmail.com>
2023-01-16 20:56:52 +0100

    Fix Preflight

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div
       style={{
-        backgroundColor: `${hex}`,
+        backgroundColor: hex,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
