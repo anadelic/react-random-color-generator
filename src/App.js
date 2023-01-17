b3fb06abe3f923507b443bbec476de16498d3a39
Ana Delic <milojevican@gmail.com>
2023-01-17 15:32:24 +0100

    Fix Index.js

--- src/App.js
+++ src/App.js
@@ -9,33 +9,29 @@ export default function App() {
   };
 
   return (
-    <>
-      <div
+    <div
+      style={{
+        backgroundColor: hex,
+        transition: '2s',
+        display: ' flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      {' '}
+      <h1 style={{ fontSize: '48px', color: 'black' }}>
+        Generated Color: {hex}
+      </h1>{' '}
+      <button
+        onClick={randomHex}
         style={{
-          backgroundColor: hex,
-          transition: '2s',
-          width: '300px',
-          height: '300px',
-          border: '2px solid black',
+          border: '1px solid black',
+          boxShadow: 'black',
         }}
       >
-        {' '}
-      </div>
-      <div>
-        <h1 style={{ fontSize: '48px', color: 'black', textAlign: 'center' }}>
-          Generated Color: {hex}
-        </h1>{' '}
-        <button
-          onClick={randomHex}
-          style={{
-            border: '1px solid black',
-            boxShadow: 'black',
-            margin: 'auto',
-          }}
-        >
-          Generate
-        </button>
-      </div>
-    </>
+        Generate
+      </button>
+    </div>
   );
 }
