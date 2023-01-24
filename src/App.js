57f0b5729507ab74b6c6db26a1188d0b452b65b0
Ana Delic <milojevican@gmail.com>
2023-01-24 13:30:50 +0100

    Change state name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-
 import { useState } from 'react';
 
 export default function App() {
-  const [hex, setHex] = useState('#ffffff');
+  const [hexColor, setHexColor] = useState('#ffffff');
   const randomHex = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    setHex(randomColor);
+    setHexColor(randomColor);
   };
 
   return (
-     <div style = {{
-      backgroundColor: '#F7F5F2',
-     display: ' flex',
-     flexDirecton: 'column',
-     justifyContent: 'center',
-     alignItems: 'center',
-     height: '100vh'}} >
+    <div
+      style={{
+        backgroundColor: '#F7F5F2',
+        display: ' flex',
+        flexDirecton: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
       <div
         style={{
-          backgroundColor: hex,
+          backgroundColor: hexColor,
           transition: '2s',
           width: '400px',
           height: '400px',
@@ -30,24 +32,28 @@ export default function App() {
           margin: '1em',
         }}
       >
-        Generated Color: {hex}
+        Generated Color: {hexColor}
       </div>
       <button
-          onClick={randomHex}
-          style={{
-              backgroundColor: '#36A9AE',
-              backgroundImage: 'linear-gradient(#37ADB2, #329CA0)',
-              border: '1px solid #2A8387',
-              borderRadius: '4px',
-              boxShadow:' rgba(0, 0, 0, 0.12) 0 1px 1px',
-              color:' #FFFFFF',
-              display: 'block',
-              fontSize: '17px',
-              lineHeight: '100%',
-              margin: '0',
-              outline: '0',
-              padding: '11px 15px 12px',
-              textAlign: 'center',
-}} > Generate </button>
-        </div>
-        );}
+        onClick={randomHex}
+        style={{
+          backgroundColor: '#36A9AE',
+          backgroundImage: 'linear-gradient(#37ADB2, #329CA0)',
+          border: '1px solid #2A8387',
+          borderRadius: '4px',
+          boxShadow: ' rgba(0, 0, 0, 0.12) 0 1px 1px',
+          color: ' #FFFFFF',
+          display: 'block',
+          fontSize: '17px',
+          lineHeight: '100%',
+          margin: '0',
+          outline: '0',
+          padding: '11px 15px 12px',
+          textAlign: 'center',
+        }}
+      >
+        Generate
+      </button>
+    </div>
+  );
+}
